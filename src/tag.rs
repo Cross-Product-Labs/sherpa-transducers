dd8eadaf76d44edc66d5a7af7f59032db7c42898
nytopop <ericizoita@gmail.com>
2025-04-03 01:13:57 -0700

    tagging streams

--- src/tag.rs
+++ src/tag.rs
@@ -1,7 +1,10 @@
+use std::collections::HashMap;
 use std::ffi::CStr;
 use std::ptr::null;
+use std::sync::Arc;
 
 use anyhow::{Result, anyhow, ensure};
+use rubato::{FftFixedIn, Resampler};
 use sherpa_rs_sys::*;
 
 use crate::{DropCString, track_cstr};
@@ -114,12 +117,13 @@ impl Config {
         let ptr = unsafe { SherpaOnnxCreateAudioTagging(&config) };
         ensure!(!ptr.is_null(), "failed to load audio tagging model");
 
-        Ok(Model { ptr, dcs: _dcs })
+        let inner = Arc::new(ModelPtr { ptr, dcs: _dcs });
+
+        Ok(Model { inner })
     }
 }
 
-/// An audio tagging model.
-pub struct Model {
+struct ModelPtr {
     ptr: *const SherpaOnnxAudioTagging,
     // NOTE: unsure if sherpa-onnx accesses these pointers post-init; we err on the side of caution and
     // keep them allocated until we drop the whole model.
@@ -128,19 +132,25 @@ pub struct Model {
 }
 
 // SAFETY: thread locals? surely not
-unsafe impl Send for Model {}
+unsafe impl Send for ModelPtr {}
 
 // SAFETY: afaik there is no interior mutability through &refs
-unsafe impl Sync for Model {}
+unsafe impl Sync for ModelPtr {}
 
-impl Drop for Model {
+impl Drop for ModelPtr {
     fn drop(&mut self) {
         unsafe { SherpaOnnxDestroyAudioTagging(self.ptr) }
     }
 }
 
+/// An audio tagging model.
+#[derive(Clone)]
+pub struct Model {
+    inner: Arc<ModelPtr>,
+}
+
 impl Model {
-    /// Create a [TaggingConfig] from a pretrained tagging model on huggingface.
+    /// Create a [Config] from a pretrained tagging model on huggingface.
     #[cfg(feature = "download-models")]
     #[cfg_attr(docsrs, doc(cfg(feature = "download-models")))]
     pub async fn from_pretrained<S: AsRef<str>>(model: S) -> Result<Config> {
@@ -187,7 +197,7 @@ impl Model {
     /// Compute audio tags in the provided samples. Tag probabilities are independent.
     pub fn tag(&self, sample_rate: usize, samples: &[f32], k: Option<usize>) -> Result<Vec<Tag>> {
         unsafe {
-            let s = SherpaOnnxAudioTaggingCreateOfflineStream(self.ptr);
+            let s = SherpaOnnxAudioTaggingCreateOfflineStream(self.inner.ptr);
             ensure!(!s.is_null(), "failed to create offline stream for tagging");
 
             SherpaOnnxAcceptWaveformOffline(
@@ -199,7 +209,7 @@ impl Model {
 
             let top_k = k.map(|k| k as i32).unwrap_or(-1);
 
-            let arr = SherpaOnnxAudioTaggingCompute(self.ptr, s, top_k);
+            let arr = SherpaOnnxAudioTaggingCompute(self.inner.ptr, s, top_k);
             ensure!(!arr.is_null(), "failed to compute audio tags");
 
             let mut i = 0;
@@ -223,10 +233,115 @@ impl Model {
             Ok(out)
         }
     }
+
+    /// Make an [OnlineStream] for incremental audio tagging.
+    pub fn online_stream(&self, in_rate: usize, chunk_size_ms: usize) -> Result<OnlineStream> {
+        let in_size = (in_rate as f32 / 1000.) * chunk_size_ms as f32;
+
+        Ok(OnlineStream {
+            model: self.clone(),
+            input: vec![],
+            fft16: FftFixedIn::new(in_rate, 16000, in_size.round() as usize, 1, 1)?,
+            buf16: vec![],
+            state: HashMap::default(),
+        })
+    }
 }
 
 #[derive(Clone, Debug)]
 pub struct Tag {
     pub name: String,
     pub prob: f32,
+}
+
+/// Context state for ~streaming audio tagging.
+///
+/// Created by [Model::online_stream].
+pub struct OnlineStream {
+    model: Model,
+    input: Vec<f32>,
+    fft16: FftFixedIn<f32>,
+    buf16: Vec<f32>,
+    state: HashMap<String, f32>,
+}
+
+impl OnlineStream {
+    /// Accept ((-1, 1)) normalized) input audio samples.
+    pub fn accept_waveform(&mut self, samples: &[f32]) {
+        self.input.extend_from_slice(samples);
+
+        let n = self.fft16.input_frames_next();
+
+        for frame in self.input.chunks_exact(n) {
+            fft_inplace(&mut self.fft16, &mut self.buf16, frame).unwrap();
+        }
+
+        keep_last_n(self.input.len() % n, &mut self.input);
+    }
+
+    /// Decode all buffered chunks.
+    pub fn decode(&mut self) -> Result<()> {
+        let n = self.fft16.input_frames_next();
+
+        for frame in self.buf16.chunks_exact(n) {
+            for tag in self.model.tag(16000, frame, None)? {
+                let p = self.state.entry(tag.name).or_insert(0.);
+                *p = p.max(tag.prob);
+            }
+        }
+
+        keep_last_n(self.buf16.len() % n, &mut self.buf16);
+
+        Ok(())
+    }
+
+    /// Returns max-pooled classification state since the last call to [OnlineStream::reset].
+    pub fn result(&self) -> Vec<(&str, f32)> {
+        let mut rs: Vec<_> = self.state.iter().map(|(k, v)| (k.as_ref(), *v)).collect();
+        rs.sort_by(|l, r| r.1.partial_cmp(&l.1).unwrap());
+        rs
+    }
+
+    /// Clear classification state and any buffered samples.
+    pub fn reset(&mut self) {
+        self.input.clear();
+        self.fft16.reset();
+        self.buf16.clear();
+        self.state.clear();
+    }
+}
+
+#[inline]
+fn fft_inplace(fft: &mut FftFixedIn<f32>, buf: &mut Vec<f32>, frame: &[f32]) -> Result<usize> {
+    let len = buf.len();
+    buf.resize(len + fft.output_frames_next(), 0.);
+    let res = fft.process_into_buffer(&[frame], &mut [&mut buf[len..]], None)?;
+    Ok(res.1)
+}
+
+#[inline]
+fn keep_last_n<T: Copy>(n: usize, buf: &mut Vec<T>) {
+    if n > 0 && n < buf.len() {
+        let src = buf.len() - n;
+        buf.copy_within(src.., 0);
+    }
+    buf.truncate(n);
+}
+
+#[test]
+fn test_keep_last_n() {
+    let mut v = vec![1, 2, 3, 4, 5, 6, 7, 8];
+
+    keep_last_n(3, &mut v);
+    assert_eq!(v, [6, 7, 8]);
+
+    keep_last_n(3, &mut v);
+    assert_eq!(v, [6, 7, 8]);
+
+    keep_last_n(0, &mut v);
+    assert!(v.is_empty());
+
+    let mut v: Vec<()> = vec![];
+    keep_last_n(0, &mut v);
+    keep_last_n(4, &mut v);
 }
